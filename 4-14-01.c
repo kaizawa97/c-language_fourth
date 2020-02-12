d0145145aaee76f2aa57564de888f01df21c6b17
kaizawawa <kai109870@gmail.com>
2020-02-12 14:59:48 +0900

    git add 4-14-01.c

--- 4-14-01.c
+++ 4-14-01.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
-int swap(int a, int b);
+#define swap(t,x,y) { tmp = x; x = y;  y = tmp; }
 
-int main () 
+int main()
 {
-	int x,y;
+	int x, y;
 	x = 11;
 	y = 2;
-	swap(x,y);
-	printf("x = %d y = %d",x,y);
+	swap(int, x, y)
+	printf("x = %d y = %d", x, y);
 }
-
-int swap(int a, int b)
-{
-	int copy = 0;
-	copy = a;
-	a = b;
-	b = copy;
-	return a,b; //二つの引数をreturnすることは無理。
-	//この場合には複数関数作らないといけないかな？
-}
-
